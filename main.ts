04a952b636b28135bbb5ef1f67b7351bc9c57a56
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2023-01-19 22:23:18 +0000

    move husky id to FOREVER - set player to 999 when button pressed

--- main.ts
+++ main.ts
@@ -1,24 +1,26 @@
 input.onButtonPressed(Button.A, function () {
+    Player = 999
+    basic.pause(1000)
     Maqueen = randint(-1, 1)
-    huskylens.request()
     if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
         Player = Rock
     } else if (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
         Player = Paper
     } else if (huskylens.isAppear(3, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
         Player = Scissors
-    } else {
-        Player = 999
     }
     if (Player != 999) {
         basic.clearScreen()
         Compare()
         if (Result == 2) {
             basic.showString("D")
+            huskylens.writeOSD("Its A Draw!", 100, 100)
         } else if (Result == Maqueen) {
             basic.showString("M")
+            huskylens.writeOSD("MaQueen Wins!", 100, 100)
         } else if (Result == Player) {
             basic.showString("P")
+            huskylens.writeOSD("Player Wins!!", 100, 100)
         }
     } else {
         basic.showIcon(IconNames.No)
@@ -55,5 +57,5 @@ Maqueen = 0
 Player = 999
 Result = 0
 basic.forever(function () {
-	
+    huskylens.request()
 })
